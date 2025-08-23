0b758aec2b9a940946965ea1bf44ba6325366e01
muhamad refaldo <dodohardcore43@gmail.com>
2025-08-23 23:47:53 +0700

    feat: update api url for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // URL backend Anda
+  baseURL: 'https://refaldo-kasir-server.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
